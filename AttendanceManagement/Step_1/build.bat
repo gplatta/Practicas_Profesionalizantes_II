a9ca65fd4f7e16b60818907973b17e53cd466eb4
GPL2020 <gplatta@gmail.com>
2021-06-26 18:31:05 -0300

    Modified - build.exe

--- AttendanceManagement/Step_1/build.bat
+++ AttendanceManagement/Step_1/build.bat
@@ -5,7 +5,7 @@ g++ -Wall -std=c++11 -c Student.cpp
 g++ -Wall -std=c++11 -c AttendanceManagement.cpp 
 cd ..
 g++ -Wall -std=c++11 -c main.cpp 
-g++ ./src/Course.o ./src/Student.o ./src/AttendanceManagement.o main.o -o Assistance.exe
+g++ ./src/Course.o ./src/Student.o ./src/AttendanceManagement.o main.o -o Attendance.exe
 
 echo "Programa compilado"
 pause
